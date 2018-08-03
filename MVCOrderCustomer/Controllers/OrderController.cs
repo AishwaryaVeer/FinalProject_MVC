0638f69bb8912cddfdf85cacf76bd67690a70e7f
nexwave <nexwave@DESKTOP-LT51R1L>
2018-08-02 22:40:56 -0700

    Add project files.

---
+++ MVCOrderCustomer/Controllers/OrderController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOrderCustomer.Models;
+
+
+namespace MVCOrderCustomer.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        OrderCustomerEntities db = new OrderCustomerEntities();
+        [HttpGet]
+        public ActionResult AddOrder()
+        {
+            var data = new SelectList(db.tblCustomers, "CustomerID", "CustomerName");
+            Session["Data"] = data;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOrder(string Command)
+        {
+            tblOrder ob = new tblOrder();
+
+            if (Command == "Save")
+            {
+                ob.ProductName = Request.Form["txtProductName"].ToString();
+                ob.Price = Decimal.Parse(Request.Form["txtPrice"].ToString());
+                ob.CustomerID = int.Parse(Request.Form["ddlCustomer"].ToString());
+                db.tblOrders.Add(ob);
+                var res = db.SaveChanges();
+                if (res > 0)
+                {
+                    ModelState.AddModelError("", "Order Added");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Could not Add Order");
+                }
+
+                return View();
+
+            }
+
+            if (Command == "Cancel")
+                return View();
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult GetOrderByName()
+        {
+            var data = new SelectList(db.tblCustomers, "CustomerID", "CustomerName");
+            Session["Data1"] = data;
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult GetOrderByName(string Show)
+        {
+
+            if (Show == "ShowOrder")
+            {
+                int id = int.Parse(Request.Form["ddlCustomer"].ToString());
+                var data = (db.tblOrders.Where(x => x.CustomerID == id)).ToList();
+
+                Session["Data2"] = data;
+
+                return View();
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Modify(int id)
+        {
+            var data = db.tblOrders.Where(x => x.OrderID == id).SingleOrDefault();
+            Session["Data3"] = data;
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Modify()
+        {
+            int id = int.Parse(Request.Form["txtid"].ToString());
+            var olddata = db.tblOrders.Where(x => x.OrderID == id).SingleOrDefault();
+            olddata.Price = Decimal.Parse(Request.Form["price"].ToString());
+
+
+            var res = db.SaveChanges();
+            if (res > 0)
+            {
+                ModelState.AddModelError("", "Order Modified");
+
+            }
+
+            return RedirectToAction("Home");
+
+           
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetOrderByID()
+        {
+           
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult GetOrderByID(string Show)
+        {
+
+            if (Show == "ShowOrder")
+            {
+                int id = int.Parse(Request.Form["id"].ToString());
+                var data = (db.tblOrders.Where(x => x.CustomerID == id)).ToList();
+
+                Session["Data5"] = data;
+
+                return View();
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+
+        public ActionResult GetCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult GetCustomer(string Show)
+        {
+            if(Show == "ADD")
+            {
+                tblCustomer n = new tblCustomer();
+                n.CustomerName=Request.Form["name"].ToString();
+                db.tblCustomers.Add(n);
+                var res = db.SaveChanges();
+
+                if (res > 0)
+                {
+                    ModelState.AddModelError("", "Customer Added! Customer ID :"+n.CustomerID);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Could not Add Customer");
+                }
+
+                return View();
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ MVCOrderCustomer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCOrderCustomer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCOrderCustomer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af47e50e-a355-4111-bccc-2a9da8e87f42")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
